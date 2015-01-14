27692076cbca8d3696cffb172d6d57f305d85866
Justin <jxm262@gmail.com>
2015-01-14 17:09:33 -0500

    refactoring

--- test/btcstats.test.js
+++ test/btcstats.test.js
@@ -16,12 +16,12 @@ describe("btcstats.js", function(){
 	describe("_exchange prop", function(){
 		
 		it("exchanges should contain all xchange listing by default", function(){
-			btcstats._exchanges.should.eql(['bitfinex', 'bitstamp', 'okcoin', 'btce', 'btc38', 'bter', 'hitbtc', 'ccex', 'Xchange']);
+			Object.keys(btcstats._exchanges).should.eql(['bitfinex', 'bitstamp', 'okcoin', 'btce', 'btc38', 'bter', 'hitbtc', 'ccex', 'Xchange']);
 		});
 		
 		it("exchanges should contain exchanges input by user if provided (subset of xchange listing)", function(){
 			btcstats.exchanges(['bitfinex', 'bitstamp', 'okcoin']);
-			btcstats._exchanges.should.eql(['bitfinex', 'bitstamp', 'okcoin']);
+			Object.keys(btcstats._exchanges).should.eql(['bitfinex', 'bitstamp', 'okcoin']);
 		});
 	});
 	

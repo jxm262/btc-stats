be021d5c95f1a1f3fc8e6ef2c09df11296e3f70f
Justin <jxm262@gmail.com>
2015-01-15 22:32:23 -0500

    Updating example file to give an idea of what the library should provide.  Committing this so I don't break my current streak on github :)

--- example/example.js
+++ example/example.js
@@ -9,7 +9,24 @@ var btcstats = require("../lib/btcstats");
 btcstats.exchanges(['bitfinex', 'bitstamp']);
 
 btcstats.avg(function(error, resp){
+	if(error){
+		console.log("errror: ");
+		console.log(error);
+	}
 	if(!error){
 		console.log(resp);
 	}
 });
+
+//btcstats.avg				// {price: xx}
+//btcstats.weightedAvg		// {price: xx}  //according to volume
+//btcstats.median			// {price: xx, exchange: xx}
+//btcstats.min				// {price: xx, exchange: xx}
+//btcstats.max				// {price: xx, same as xchange}
+//btcstats.minVolume		// {price: xx, same as xchange}
+//btcstats.maxVolume		// {price: xx, same as xchange}
+//btcstats.maxSpread		// {spread: xx, ask: xx (low) , bid: xx (high), askExchange: xx, bidExchange: xx}
+//btcstats.minSpread		// {spread: xx, ask: xx (low) , bid: xx (high), askExchange: xx, bidExchange: xx}
+
+
+

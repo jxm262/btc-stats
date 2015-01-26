fa0ff5b540cd670a967988bf4f5dd7cf03ab323e
Justin <jxm262@gmail.com>
2015-01-26 14:05:16 -0500

    fixing examples

--- example/example.js
+++ example/example.js
@@ -2,91 +2,103 @@
  * Example running functions on 3 exchanges (bitfinex, bitstamp, okcoin)
  */
 
-"use strict";
+//"use strict";
 
-var btcstats = require("../lib/btcstats");
+var btcstats = require("../lib/btcstats")
 
-btcstats.exchanges(['bitfinex', 'bitstamp', 'okcoin']);
+//btcstats.exchanges(['bitfinex', 'bitstamp', 'okcoin']);
 
-function print(error, resp){
-	if(error){
-		console.log("error: " + error);
-	} else{
+
+btcstats.avg(function(error, resp) {
+	if (!error) {
+		console.log("\n avg: ");
 		console.log(resp);
 	}
-}
-
-
-btcstats.avg(function(error, resp){
-	console.log("avg: ");
-	print(error, resp);
 });
 
 
 btcstats.weightedAvg(function(error, resp){
-	console.log("weightedAvg: ");
-	print(error, resp);
+	if (!error) {
+		console.log("\n weightedAvg: ");
+		console.log(resp);
+	}
 });
 
 
 btcstats.min(function(error, resp){
-	console.log("min");
-	print(error, resp);
+	if (!error) {
+		console.log("\n min: ");
+		console.log(resp);
+	}
 });
 
 
 btcstats.max(function(error, resp){
-	console.log("max: ");
-	print(error, resp);
+	if (!error) {
+		console.log("\n max: ");
+		console.log(resp);
+	}
 });
 
 
 btcstats.minVolume(function(error, resp){
-	console.log("minVolume: ");
-	print(error, resp);
+	if (!error) {
+		console.log("\n minVolume: ");
+		console.log(resp);
+	}
 });
 
 
 btcstats.maxVolume(function(error, resp){
-	console.log("maxVolume: ");
-	print(error, resp);
+	if (!error) {
+		console.log("\n maxVolume: ");
+		console.log(resp);
+	}
 });
 
 
 btcstats.minSpread(function(error, resp){
-	console.log("minSpread: ");
-	print(error, resp);
+	if (!error) {
+		console.log("\n minSpread: ");
+		console.log(resp);
+	}
 });
 
 
 btcstats.maxSpread(function(error, resp){
-	console.log("maxSpread: ");
-	print(error, resp);
+	if (!error) {
+		console.log("\n maxSpread: ");
+		console.log(resp);
+	}
 });
 
 
 btcstats.exchangeMinSpread(function(error, resp){
-	console.log("exchangeMinSpread: ");
-	print(error, resp);
+	if (!error) {
+		console.log("\n exchangeMinSpread: ");
+		console.log(resp);
+	}
 });
 
 
 btcstats.exchangeMaxSpread(function(error, resp){
-	console.log("exchangeMaxSpread");
-	print(error, resp);
+	if (!error) {
+		console.log("\n exchangeMaxSpread: ");
+		console.log(resp);
+	}
 });
 
 
-//TODO: think of function name to differentiate the 
-//   -exchange with highest ask vs exchange with lowest bid
-//   vs
-//   -exchange with the highest ask vs lowest bid
-
-//across all exchanges (example - the spread difference across all exchanges, return highest ask, lowest bid)
-//btcstats.maxSpread		// {spread: xx, ask: xx (low) , bid: xx (high), askExchange: xx, bidExchange: xx}
-//btcstats.minSpread		// {spread: xx, ask: xx (low) , bid: xx (high), askExchange: xx, bidExchange: xx}
-
-//across single exchanges  (example the exchange with the max spread)
-//btcstats.exchangeMaxSpread		// {spread: xx, ask: xx (low) , bid: xx (high), exchange: xx}
-//btcstats.exchangeMinSpread		// {spread: xx, ask: xx (low) , bid: xx (high), exchange: xx}
-
+////TODO: think of function name to differentiate the 
+////   -exchange with highest ask vs exchange with lowest bid
+////   vs
+////   -exchange within the list (all exchanges) with the highest ask vs lowest bid
+//
+////across all exchanges (example - the spread difference across all exchanges, return highest ask, lowest bid)
+////btcstats.maxSpread		// {spread: xx, ask: xx (low) , bid: xx (high), askExchange: xx, bidExchange: xx}
+////btcstats.minSpread		// {spread: xx, ask: xx (low) , bid: xx (high), askExchange: xx, bidExchange: xx}
+//
+////across single exchanges  (example the exchange with the max spread)
+////btcstats.exchangeMaxSpread		// {spread: xx, ask: xx (low) , bid: xx (high), exchange: xx}
+////btcstats.exchangeMinSpread		// {spread: xx, ask: xx (low) , bid: xx (high), exchange: xx}
+//
